1d88fd31e1f62e37309324f1d941e18ea725b8d2
okuzono <mako.to.momo.3762293@gmail.com>
2025-06-19 21:46:50 +0900

    事前テスト①-1～①-5

---
+++ example.py
@@ -0,0 +1,25 @@
+#①-1
+print("hello world")
+
+#①-2
+def greet():
+    print("こんにちは")
+greet()
+
+#①-3
+def print_name(name):
+    print(f"私の名前は{name}です")
+print_name("ライト")
+
+#①-4
+def get_greet():
+     return "おはようございます"
+morning = get_greet()
+print(morning)
+
+#①-5
+def add(a,b):
+    return a + b
+answer = add(12,34)
+print(answer)
+
